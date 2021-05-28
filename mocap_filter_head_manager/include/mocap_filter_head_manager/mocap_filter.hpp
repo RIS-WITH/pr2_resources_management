db48ed4b791bfad4b4d159d45f4cccff253bc215
guilhembn <gbuisan@laas.fr>
2021-05-28 17:12:34 +0200

    [MocapFilter] Sets priority based on distance to settable frames

--- mocap_filter_head_manager/include/mocap_filter_head_manager/mocap_filter.hpp
+++ mocap_filter_head_manager/include/mocap_filter_head_manager/mocap_filter.hpp
@@ -5,14 +5,21 @@
 #include <optitrack_ros/or_pose_estimator_state.h>
 #include <geometry_msgs/PointStamped.h>
 #include <dynamic_reconfigure/server.h>
+#include <tf2_ros/transform_listener.h>
 
 #define PUBLISH_PERIOD_PARAM_NAME "publish_period"
 #define NO_DATA_TIMEOUT_PARAM_NAME "no_data_timeout"
+#define STANDARD_DIST_PARAM_NAME "standard_priority_distance"
+#define HIGH_DIST_PARAM_NAME "high_priority_distance"
+#define DISTANCE_FRAMES_PARAM_NAME "distance_frames"
 
 
 struct Params{
     double publishPeriod;
     double noDataTimeout;
+    double standardDistSq;
+    double highDistSq;
+    std::vector<std::string> distanceFrames;
 };
 
 class MocapFilter{
@@ -22,6 +29,7 @@ class MocapFilter{
 
     void onNewPoint(const optitrack_ros::or_pose_estimator_stateConstPtr& msg);
     void publishPoint(const ros::TimerEvent& e);
+    double getMinSqDistancesLastPointToFrames() const;
 
     protected:
     void updateParams();
@@ -31,7 +39,9 @@ class MocapFilter{
     ros::Timer pubTimer_;
     geometry_msgs::PointStamped lastPoint_;
     ros::Time lastPointTime_;
-    Params params_; 
+    Params params_;
+    tf2_ros::Buffer tfBuffer_;
+    std::unique_ptr<tf2_ros::TransformListener> tfListener_;
 };
 
 #endif /* MOCAP_FILTER_HPP */
--- mocap_filter_head_manager/src/mocap_filter.cpp
+++ mocap_filter_head_manager/src/mocap_filter.cpp
@@ -1,9 +1,14 @@
 #include "mocap_filter.hpp"
 #include <pr2_head_manager_msgs/Point.h>
+#include <tf2/LinearMath/Vector3.h>
+#include <tf2/convert.h>
+#include <tf2/LinearMath/Transform.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 
 MocapFilter::MocapFilter(ros::NodeHandlePtr& nh): nh_(nh){
     updateParams();
+    tfListener_ = std::unique_ptr<tf2_ros::TransformListener>(new tf2_ros::TransformListener(tfBuffer_));
     headManagerPub_ = nh_->advertise<pr2_head_manager_msgs::Point>("/head_manager_output", 1);
     optitrackSub_ = nh_->subscribe("/optitrack_input_topic", 1, &MocapFilter::onNewPoint, this);
     pubTimer_ = nh_->createTimer(ros::Duration(params_.publishPeriod), &MocapFilter::publishPoint, this);
@@ -36,23 +41,58 @@ void MocapFilter::publishPoint(const ros::TimerEvent& e){
         headManagerPub_.publish(pt);
         return;
     }
-    pt.priority.value = pt.priority.STANDARD;  // TODO: something else?
+
+    double minDistSq = getMinSqDistancesLastPointToFrames();
+    if (minDistSq <= params_.highDistSq){
+        pt.priority.value = pt.priority.HIGH;
+    }else if (minDistSq <= params_.standardDistSq){
+        pt.priority.value = pt.priority.STANDARD;
+    }else{
+        pt.priority.value = pt.priority.LOW;
+    }
     pt.data = lastPoint_;
     headManagerPub_.publish(pt);
     updateParams();    
 }
 
 void MocapFilter::updateParams(){
-    Params p;
-    nh_->param(PUBLISH_PERIOD_PARAM_NAME, p.publishPeriod, 0.1);
-    nh_->param(NO_DATA_TIMEOUT_PARAM_NAME, p.noDataTimeout, 2.0);
 
-    if (p.publishPeriod != params_.publishPeriod){
-        pubTimer_ = nh_->createTimer(p.publishPeriod, &MocapFilter::publishPoint, this);
-        params_ = p;
-    }else if (p.noDataTimeout != params_.noDataTimeout){
-        params_ = p;
+    double oldPublishPeriod = params_.publishPeriod;
+    nh_->param(PUBLISH_PERIOD_PARAM_NAME, params_.publishPeriod, 0.1);
+    nh_->param(NO_DATA_TIMEOUT_PARAM_NAME, params_.noDataTimeout, 2.0);
+    nh_->param(STANDARD_DIST_PARAM_NAME, params_.standardDistSq, 3.0);
+    nh_->param(HIGH_DIST_PARAM_NAME, params_.highDistSq, 1.0);
+    nh_->param(DISTANCE_FRAMES_PARAM_NAME, params_.distanceFrames, {"base_footprint"});
+
+    params_.standardDistSq *= params_.standardDistSq;
+    params_.highDistSq *= params_.highDistSq;
+
+    if (params_.publishPeriod != oldPublishPeriod){
+        pubTimer_ = nh_->createTimer(params_.publishPeriod, &MocapFilter::publishPoint, this);
     }
+}
+
+double MocapFilter::getMinSqDistancesLastPointToFrames() const{
+    geometry_msgs::TransformStamped frame2optitrack;
+    tf2::Transform frame2optitrackTf;
+    tf2::Vector3 point, result;
+    tf2::fromMsg(lastPoint_.point, point);
+    double minSqDist = 9999999.0;
+    for (const std::string& frame: params_.distanceFrames){
+        try{
+        frame2optitrack = tfBuffer_.lookupTransform(frame, lastPoint_.header.frame_id,
+                                    ros::Time(0));
+        }
+        catch (tf2::TransformException &ex) {
+            ROS_WARN_STREAM("Can't transform the origin frame of the point of interest: '" << lastPoint_.header.frame_id 
+                        << "' into the frame specified in parameter: '" << frame << "'. Exception: " << ex.what());
+            continue;
+        }
+        tf2::fromMsg(frame2optitrack.transform, frame2optitrackTf);
+        result = frame2optitrackTf * point;
+        minSqDist = std::min(minSqDist, result.length2());
+    }
+    return minSqDist;
 }
 
 
