041dba2897ada3e1fdc8d913e2b2eeb6a57f24de
Guillaume Sarthou <gsarthou@laas.fr>
2022-10-04 16:27:35 +0200

    [Pr2HeadManager] remove debug

--- pr2_head_manager/src/pr2_head_manager.cpp
+++ pr2_head_manager/src/pr2_head_manager.cpp
@@ -238,7 +238,6 @@ void Pr2HeadManager::checkGoalActive() {
         if (head_state != actionlib::SimpleClientGoalState::ACTIVE){
             this->done();
             _is_command_running = false;
-            std::cout << "done" << std::endl;
         }
     }else{
         if (head_state == actionlib::SimpleClientGoalState::ACTIVE){
